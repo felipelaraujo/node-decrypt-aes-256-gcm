6f0e4c973c7764e15debfc18cfc3459a0332eed5
Felipe Araújo <felipe.lopes@labsit.io>
2022-09-16 18:04:35 -0300

    Added express

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Decrypt aes-256-gcm with Node.js
+
+### How to run the project:
+```bash
+$ npm start
+```
--- index.js
+++ index.js
@@ -1,35 +1,43 @@
+const express = require("express");
+const app = express();
+const port = 3000;
 const crypto = require("crypto");
+const bodyParser = require("express");
 
-const requestExample = {
-  encoded:
-    "Pe7qFBgnOWQOVsiI9PG9h9h3kS6D3kV5b9uAM+E7+ac48nK9LyXK6N4CwAEFw20nvQhbrjMqHOF4aJPhkihwbJxVnfegHNc8qYJa/nfEu4XQdSR1lJYhnN6glhyih5onsBsAI6MFxh07xC/NJMoCKyoCDP1utAKQTGr1mjKtjch2ovshYza3v3PD06TdAjIOoc7oPB7c86uKM3uZP65wjofU2jqdLZO1BRFC/kb10IKmT9lFFd0LNK/m03JyNE64AJkCjMp9I4/jaR5AWcAqwC5P7LMRVQiQ84BSzvXj+IFCd71ZGM9qRYsOWcSPhM0hv+18JdTOF8ZrFkoa0pCzXG65v1BHYFKq2thdltHn4UUm8qmmiV5qxSjJAAQDl536OLojGpYRB1TQ",
-  iv: "EuThRs6pAsTq3e2C",
-  tag: "Q7RbbOIcJWr6m1lhcfcM0g==",
-  secret: "OcMtPtCI2Dgs3saUGqGOIrKJvydBxp/5E6C/58dGtao=",
-};
+app.use(bodyParser.text({ type: "*/*" }));
 
-try {
-  const encoded = Buffer.from(requestExample.encoded, "base64").toString("hex");
+app.listen(port, () => {
+  console.info("- App listening at port: ", port);
+});
 
-  const iv = Buffer.from(requestExample.iv, "base64").toString("hex");
+app.post("/hook", (req, res) => {
+  try {
+    const encodedMessage = req.body;
+    const authenticationTag = req.headers["x-authentication-tag"];
+    const initializationVector = req.headers["x-initialization-vector"];
+    const secretKey = req.headers["secret-key"];
 
-  const authTag = Buffer.from(requestExample.tag, "base64").toString("hex");
+    const encoded = Buffer.from(encodedMessage, "base64").toString("hex");
+    const authTag = Buffer.from(authenticationTag, "base64").toString("hex");
+    const iv = Buffer.from(initializationVector, "base64").toString("hex");
+    const secret = Buffer.from(secretKey, "base64").toString("hex");
 
-  const secret = Buffer.from(requestExample.secret, "base64").toString("hex");
+    const decipher = crypto.createDecipheriv(
+      "aes-256-gcm",
+      Buffer.from(secret, "hex"),
+      Buffer.from(iv, "hex")
+    );
 
-  const decipher = crypto.createDecipheriv(
-    "aes-256-gcm",
-    Buffer.from(secret, "hex"),
-    Buffer.from(iv, "hex")
-  );
+    decipher.setAuthTag(Buffer.from(authTag, "hex"));
 
-  decipher.setAuthTag(Buffer.from(authTag, "hex"));
+    let decrypted = decipher.update(encoded, "hex", "utf8");
 
-  let decrypted = decipher.update(encoded, "hex", "utf8");
+    decrypted = `${decrypted}${decipher.final("utf8")}`;
 
-  decrypted = `${decrypted}${decipher.final("utf8")}`;
-
-  console.log({ DecyptedMessage: JSON.parse(decrypted) });
-} catch (error) {
-  console.log(error);
-}
+    res.status(200);
+    res.send(JSON.parse(decrypted));
+  } catch (error) {
+    res.status(400);
+    res.send(error);
+  }
+});
